51e71ec1cd42e0d2dcb234ed4e79efc698bfa6ee
liamhuynh98 <liamhuynh21@gmail.com>
2022-08-20 02:10:15 -0500

    qualification__active is not working, need fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Menu show hidden
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 //show menu
 //check to see if ID exist exist
@@ -17,14 +17,53 @@ if (navClose) {
   navMenu.classList.remove("show-menu");
 }
 
-//mobile menu exit 
-const navLink = document.querySelectorAll('.nav__link');
+//mobile menu exit
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction () {
-  const navMenu = document.getElementById('nav-menu');
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
 
-  //clicking on nav__link will remove the show-menu class 
-  navMenu.classList.remove('show-menu')
+  //clicking on nav__link will remove the show-menu class
+  navMenu.classList.remove("show-menu");
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*skills cordination*/
+const skillsContent = document.getElementsByClassName("skills__content");
+const skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
+
+/*qualification tabs*/
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[data-content");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.classList.remove("qualification__active");
+  });
+
+  target.classList.add("qualification__active");
+
+  tabs.forEach((tab) => {
+    tab.classList.remove("qualification__active");
+  });
+
+  tab.classList.add("qualification__active");
+});
